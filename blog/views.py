23da47f1a3d6b325fe2b7af07304ee4d5503351c
hsrowl <hashire.owl@gmail.com>
2018-03-04 18:01:42 -0800

    second commit

--- blog/views.py
+++ blog/views.py
@@ -1,20 +1,51 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Post, Category
-from comments.forms import CommentForm
 import markdown
-# Create your views here.
+
+from django.shortcuts import render, get_object_or_404
+from comments.forms import CommentForm
+from .models import Post, Category
+
+"""
+请使用下方的模板引擎方式。
+def index(request):
+    return HttpResponse("欢迎访问我的博客首页！")
+"""
+
+"""
+请使用下方真正的首页视图函数
+def index(request):
+    return render(request, 'blog/index.html', context={
+        'title': '我的博客首页',
+        'welcome': '欢迎访问我的博客首页'
+    })
+"""
+
 
 def index(request):
-    post_list = Post.objects.all().order_by('-created_time')
+    post_list = Post.objects.all()
     return render(request, 'blog/index.html', context={'post_list': post_list})
 
+
+"""
+请使用下方包含评论列表和评论表单的详情页视图
 def detail(request, pk):
-    post = get_object_or_404(Post,pk=pk)
+    post = get_object_or_404(Post, pk=pk)
     post.body = markdown.markdown(post.body,
                                   extensions=[
-                                     'markdown.extensions.extra',
-                                     'markdown.extensions.codehilite',
-                                     'markdown.extensions.toc',
+                                      'markdown.extensions.extra',
+                                      'markdown.extensions.codehilite',
+                                      'markdown.extensions.toc',
+                                  ])
+    return render(request, 'blog/detail.html', context={'post': post})
+"""
+
+
+def detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.body = markdown.markdown(post.body,
+                                  extensions=[
+                                      'markdown.extensions.extra',
+                                      'markdown.extensions.codehilite',
+                                      'markdown.extensions.toc',
                                   ])
     # 记得在顶部导入 CommentForm
     form = CommentForm()
@@ -26,16 +57,17 @@ def detail(request, pk):
                'form': form,
                'comment_list': comment_list
                }
-    return render(request, 'blog/detail.html', context={'post': post})
+    return render(request, 'blog/detail.html', context=context)
+
 
 def archives(request, year, month):
     post_list = Post.objects.filter(created_time__year=year,
                                     created_time__month=month
-                                    ).order_by('-created_time')
+                                    )
     return render(request, 'blog/index.html', context={'post_list': post_list})
 
+
 def category(request, pk):
-    # 记得在开始部分导入 Category 类
     cate = get_object_or_404(Category, pk=pk)
-    post_list = Post.objects.filter(category=cate).order_by('-created_time')
+    post_list = Post.objects.filter(category=cate)
     return render(request, 'blog/index.html', context={'post_list': post_list})
--- blogproject/settings.py
+++ blogproject/settings.py
@@ -120,5 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
-# 加入下面的配置
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
